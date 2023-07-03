be7a97c9095538b439bd8d4760495ace5f92901c
Svetlana <pidgin66@gmail.com>
2023-07-03 11:20:21 +0300

    fix eslint error

--- src/page_loader.js
+++ src/page_loader.js
@@ -1,15 +1,13 @@
-import fsp from "fs/promises";
-import axios from "axios";
-import url from "url";
-import { cwd } from "process";
-import nameFile from "./renameUrl.js";
+import fsp from 'fs/promises';
+import axios from 'axios';
+import url from 'url';
+import { cwd } from 'process';
+import nameFile from './renameUrl.js';
 
 const readUrl = (link, path = process.cwd()) => {
   const read = axios
     .get(link)
-    .then((html) =>
-      fsp.writeFile(path.concat("/", nameFile(link)), html.data)
-    );
+    .then((html) => fsp.writeFile(path.concat('/', nameFile(link)), html.data));
   return read;
 };
 
--- src/renameUrl.js
+++ src/renameUrl.js
@@ -1,9 +1,9 @@
 const nameFile = (link) => {
   const { hostname, pathname } = new URL(link);
   const regex = /\W/g;
-  const name = hostname.concat(pathname).replace(regex, "-").concat(".html");
+  const name = hostname.concat(pathname).replace(regex, '-').concat('.html');
 
   return name;
 };
 
-export  default  nameFile;
+export default nameFile;
