fea7e48000add3c0811372ff7e6483f35fb64e85
Svetlana Zinovkina <75115284+SvetlanaZinovkina@users.noreply.github.com>
2023-09-08 10:10:07 +0000

    refactoring

--- src/utilities.js
+++ src/utilities.js
@@ -1,40 +1,65 @@
+import path from 'path';
+import fs from 'fs/promises';
+import * as cheerio from 'cheerio';
 import axios from 'axios';
-import debug from 'debug';
 
-export const createFileName = (url, fileType) => {
-  const name = new URL(url);
-  const fileName = name
-    .toString()
-    .replace(`${name.protocol}//`, '')
-    .replace(fileType, '')
-    .replace(/[^a-zA-Z0-9]/g, '-');
-  return `${fileName}${fileType}`;
+const changeUrl = (url) => url.replace(/\W+/g, '-').replace(/-+$/, '');
+
+export const urlToFilename = (url) => {
+  const { pathname, hostname } = url;
+  const { ext, dir, name } = path.parse(pathname);
+  const filename = changeUrl(`${hostname}/${dir}/${name}`);
+  const fileExtension = ext || '.html';
+
+  return `${filename}${fileExtension}`;
 };
 
-export const getFullLink = (relativeUrl, baseUrl) => {
-  const url = new URL(relativeUrl, baseUrl);
-  return url.toString();
+export const urlToDirname = (url) => {
+  const { pathname, hostname } = url;
+  const { dir, name } = path.parse(pathname);
+  const dirname = changeUrl(`${hostname}/${dir}/${name}`);
+
+  return `${dirname}_files`;
 };
 
-export const replaceLinks = (links, html, assetsPath, baseUrl) => {
-  let result = html;
+export const extractAssets = (data, pageUrl, dirName) => {
+  const tagsAttributes = {
+    img: 'src',
+    link: 'href',
+    script: 'src',
+  };
 
-  links.forEach((link) => {
-    const fileType = link.includes('.') ? `.${link.split('.').pop()}` : '.html';
-    const fullLink = getFullLink(link, baseUrl);
-    const fileName = createFileName(fullLink, fileType);
+  const { origin } = pageUrl;
+  const $ = cheerio.load(data);
+  const assets = Object.entries(tagsAttributes)
+    .flatMap(([tagName, attribute]) => $(`${tagName}[${attribute}]`)
+      .toArray()
+      .map((element) => {
+        const $element = $(element);
+        const src = $element.attr(attribute);
+        const assetUrl = new URL(src, origin);
+        const name = urlToFilename(assetUrl);
 
-    result = result.replace(link, `${assetsPath}/${fileName}`).replace(' />', '>');
-  });
+        return {
+          $element, assetUrl, attribute, name,
+        };
+      }))
+    .filter(({ assetUrl }) => assetUrl.origin === origin)
+    .map(({
+      $element, assetUrl, attribute, name,
+    }) => {
+      $element.attr(attribute, `${dirName}/${name}`);
 
-  return result;
+      return { assetUrl, name };
+    });
+
+  const html = $.root().html();
+
+  return { html, assets };
 };
 
-export const isSameDomain = (url, baseUrl) => {
-  const link = new URL(url, baseUrl);
-  return link.hostname === new URL(baseUrl).hostname || url.startsWith('/');
-};
+export const writeFile = (filePath, content) => fs.writeFile(filePath, content);
 
-const logAxios = debug('axios');
-
-export const getHTML = (url) => axios.get(url).catch((e) => logAxios(e));
+export const downloadAsset = (pageUrl, assetPath) => axios
+  .get(pageUrl, { responseType: 'arraybuffer' })
+  .then((response) => writeFile(assetPath, response.data));
